22ce3d033a2b51ca9d7bcb12869b47e97e9c29e5
Abdullah Fawzi <abdullahfawzi1684@gmail.com>
2023-09-14 16:34:24 +0300

    flutter_cli installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:aflam/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -12,7 +14,9 @@ final theme = ThemeData(
   textTheme: GoogleFonts.latoTextTheme(),
 );
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
