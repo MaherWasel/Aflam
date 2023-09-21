9f4cf8ab6d2f965f55bf6d3eddb986cf7d12f592
abdullah1684 <abdullahfawzi1684@gmail.com>
2023-09-21 18:31:45 +0300

    finish profile picture

--- lib/Widgets/HomeDrawer.dart
+++ lib/Widgets/HomeDrawer.dart
@@ -1,13 +1,47 @@
+import 'dart:io';
+
 import 'package:aflam/Screens/AboutUs.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
-class HomeDrawer extends StatelessWidget{
+class HomeDrawer extends StatefulWidget{
   const HomeDrawer({super.key});
 
   @override
+  State<HomeDrawer> createState() => _HomeDrawerState();
+}
+
+
+class _HomeDrawerState extends State<HomeDrawer> {
+  final currentUser = FirebaseAuth.instance.currentUser!;
+  var _imageIsOptained = false;
+  File? _optainedImage;
+@override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async{
+    final userData = await FirebaseFirestore.instance.collection('users').doc(currentUser.uid).get();
+    
+    if(userData.data()!['imageUrl'] != null){
+       setState(() {
+      _imageIsOptained = true;
+    });
+    }
+   
+
+    _optainedImage = userData.data()!["imageUrl"];
+  }
+
+  
+  
+  @override
   Widget build(BuildContext context) {
+    
     return Drawer(
       width: 220,
       child: Column(
@@ -24,10 +58,10 @@ class HomeDrawer extends StatelessWidget{
                   Theme.of(context).colorScheme.onInverseSurface.withOpacity(0.75)
                 ])
             ),
-            child: const SizedBox(
-              width: double.infinity,
-              height: 150,
-              child: Icon(Icons.person_2_rounded,size: 100,))),
+            child: CircleAvatar(
+              radius: 90,
+              backgroundImage: FileImage(_optainedImage!),)
+              ),
           const SizedBox(
             height: 6,
           ),
@@ -59,5 +93,4 @@ class HomeDrawer extends StatelessWidget{
         ]),
     );
   }
-
 }
